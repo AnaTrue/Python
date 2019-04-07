e912901bef2ca4f9deb28c3b02783021cde3ed1f
Trushina <truegirl96@mail.ru>
2019-04-07 23:29:51 +0300

    Выполнила 2 задания из первого уровня. Я не совсем всё понимаю, поэтому вникать буду медленнее.

---
+++ Урок 2/1.1.py
@@ -0,0 +1,5 @@
+list1 = ["яблоко", "банан", "киви", "арбуз"]
+print("1: ", '{:>10}'.format(list1[0]))
+print("2: ", '{:>10}'.format(list1[1]))
+print("3: ", '{:>10}'.format(list1[2]))
+print("4: ", '{:>10}'.format(list1[3]))
